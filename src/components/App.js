0766475008e53ff0298d72392c086207ce77af2f
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-06 17:10:12 -0600

    Connected reduced to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,75 @@
-import React from 'react';
+// Import in 'useReducer' to gain access to the reducer. *******************************************************
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+// Imported the reducer and the initial state *******************************************************
+import reducer, { initialState } from '../reducers/index';
+
 function App() {
-  return (
-    <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"> Reducer Challenge</a>
-      </nav>
+     // Set up tht state for the app *******************************************************
+     // This assignes state from initialState
+     const [state, dispatch] = useReducer(reducer, initialState);
 
-      <div className = "container row mt-5">
-        <div className="col-md-12 d-flex justify-content-center">
-          <form name="Cal">
-            
-            <TotalDisplay value={0}/>
-            <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
-            </div>
+     return (
 
-            <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
-            </div>
+          <div className="App">
+               <nav className="navbar navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#"> Reducer Challenge</a>
+               </nav>
 
-            <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
-            </div>
+               <div className="container row mt-5">
+                    <div className="col-md-12 d-flex justify-content-center">
+                         <form name="Cal">
 
-            <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
-            </div>
+                              <TotalDisplay value={state.total} />
+                              <div className="row details">
+                                   <span id="operation"><b>Operation:</b>{state.operation}</span>
+                                   <span id="memory"><b>Memory:</b>{state.memory}</span>
+                              </div>
 
-            <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
-            </div>
+                              <div className="row">
+                                   <CalcButton value={"M+"} />
+                                   <CalcButton value={"MR"} />
+                                   <CalcButton value={"MC"} />
+                              </div>
 
-            <div className="row ce_button">
-              <CalcButton value={"CE"}/>
-            </div>
+                              <div className="row">
+                                   <CalcButton value={1} />
+                                   <CalcButton value={2} />
+                                   <CalcButton value={3} />
+                              </div>
 
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+                              <div className="row">
+                                   <CalcButton value={4} />
+                                   <CalcButton value={5} />
+                                   <CalcButton value={6} />
+                              </div>
+
+                              <div className="row">
+                                   <CalcButton value={7} />
+                                   <CalcButton value={8} />
+                                   <CalcButton value={9} />
+                              </div>
+
+                              <div className="row">
+                                   <CalcButton value={"+"} />
+                                   <CalcButton value={"*"} />
+                                   <CalcButton value={"-"} />
+                              </div>
+
+                              <div className="row ce_button">
+                                   <CalcButton value={"CE"} />
+                              </div>
+
+                         </form>
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default App;
